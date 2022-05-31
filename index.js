76764fafb7a9d498fe9dd44efa2473ff27f218d7
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-05-31 16:50:54 -0500

    added functions to lib javascript files and some to the index.js

--- index.js
+++ index.js
@@ -15,13 +15,47 @@ function teamMenu(){
                 },
                 {
                     type: 'input',
-                    name:'managerEmail',
-                    message:`What is your manager's name?`
+                    name:'managerId',
+                    message:`What is your manager's id?`
                 },
+                {
+                    type: 'input',
+                    name:'managerEmail',
+                    message:`What is your manager's email?`
+                },
+                {
+                    type: 'input',
+                    name:'managerOfficeNumber',
+                    message:`What is your manager's office number?`
+                }
             ]) 
 
             
     } 
+
+    function engineerInfo(){
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name:'name',
+                    message:`What is your engineer's name?`
+                },
+                {
+                    type: 'input',
+                    name:'email',
+                    message:`What is your engineer's email?`
+                },
+                {
+                    type: 'input',
+                    name:'github',
+                    message:`What is your github profile link?`
+                }
+            ]) 
+
+            
+    } 
+
     managerInfo();
 }
 
