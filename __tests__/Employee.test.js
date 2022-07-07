50411273c2f963d19249b64ef27c17cf5ffdf1e7
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-07-07 16:53:37 -0500

    had to restart node since it wasn't recognizing inquirer due to a more recent version of node; updated Employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+//variables for checking Employee object
+const checkEmployee = require('../lib/Engineer');
+const employee = new checkEmployee('julia','123456','juliat@gmail.com');
+
+test('checks if Employee values matches', () => {
+ expect(employee.name).toBe('julia');
+ expect(employee.email).toBe('juliat@gmail.com');
+ expect(employee.id).toBe('123456');
+
+});
+
+test('checks name within object',() => {
+    expect(employee.getName()).toBe('julia');
+});
+
+test('checks employee id within object',() => {
+    expect(employee.getId()).toBe('123456');
+});
+
+test('checks email within object',() => {
+    expect(employee.getEmail()).toBe('juliat@gmail.com');
+});
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
+// import inquirer from 'inquirer';
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { type } = require("os");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 //prompts 
 
